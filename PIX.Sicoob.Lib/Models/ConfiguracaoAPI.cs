63d5556ba0eac315f2f20c6832500ffb4025129a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-19 16:54:27 -0300

    Primeiro commit

---
+++ PIX.Sicoob.Lib/Models/ConfiguracaoAPI.cs
@@ -0,0 +1,15 @@
+﻿namespace PIX.Sicoob.Lib.Models
+{
+    public class ConfiguracaoAPI
+    {
+        public string ClientId { get; set; }
+
+        public string UrlAutenticacao { get; set; } = "https://auth.sicoob.com.br/auth/realms/cooperado/protocol/openid-connect/";
+        public string UrlCertificadoPFX { get; set; }
+        public string CertificadoSenha { get; set; }
+
+
+        public string UrlApi { get; set; } = "https://api.sicoob.com.br/";
+
+    }
+}
---
+++ PIX.Sicoob.Lib/SicoobAPI.cs
@@ -0,0 +1,24 @@
+﻿
+using Simple.API;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PIX.Sicoob.Lib
+{
+    public class SicoobAPI
+    {
+        private readonly Models.ConfiguracaoAPI config;
+        private ClientInfo clientAuth;
+        private ClientInfo clientApi;
+
+        public SicoobAPI(Models.ConfiguracaoAPI config)
+        {
+            this.config = config ?? throw new System.ArgumentNullException(nameof(config));
+        }
+
+        public async Task SetupAsync()
+        {
+        }
+
+    }
+}
