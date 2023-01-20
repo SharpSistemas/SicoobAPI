75fdc27ea026333643b8011a924decac88fd3521
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-20 09:47:02 -0300

    Ajusta teste

--- PIX.Sicoob.UnitTest/ExtensionsTests/ToKvpTests.cs
+++ PIX.Sicoob.UnitTest/ExtensionsTests/ToKvpTests.cs
@@ -11,8 +11,8 @@ namespace PIX.Sicoob.UnitTest.ExtensionsTests
         {
             var cons = new Lib.Models.Pix.ConsultaRequest()
             {
-                inicio = new System.DateTime(2020, 01, 01),
-                fim = new System.DateTime(2021, 12, 31),
+                inicio = new System.DateTime(2020, 01, 01, 0, 0, 0, System.DateTimeKind.Utc),
+                fim = new System.DateTime(2021, 12, 31, 0, 0, 0, System.DateTimeKind.Utc),
 
                 cnpj = "17189722000139",
 
@@ -26,7 +26,7 @@ namespace PIX.Sicoob.UnitTest.ExtensionsTests
             var kvp = cons.ToKVP();
             var str = string.Join(';', kvp.Select(p => $"{p.Key}={p.Value}"));
 
-            var expected = "inicio=2020-01-01T00:00:00.000-03:00;fim=2021-12-31T00:00:00.000-03:00;cnpj=17189722000139;paginacao.paginaAtual=0;paginacao.itensPorPagina=200";
+            var expected = "inicio=2020-01-01T00:00:00.000+00:00;fim=2021-12-31T00:00:00.000+00:00;cnpj=17189722000139;paginacao.paginaAtual=0;paginacao.itensPorPagina=200";
 
             Assert.That(str, Is.EqualTo(expected));
         }
