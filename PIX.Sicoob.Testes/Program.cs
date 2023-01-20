b170160e2ccefbb61f6bd08d8960154acfc506fb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-20 09:44:27 -0300

    Program de testes

---
+++ PIX.Sicoob.Testes/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using PIX.Sicoob.Lib;
+using PIX.Sicoob.Lib.Models;
+using System.IO;
+
+var cfg = JsonConvert.DeserializeObject<ConfiguracaoAPI>(File.ReadAllText("config.json"));
+// save (atualiza cfg)
+//File.WriteAllText("config.json", JsonConvert.SerializeObject(cfg));
+
+var sicoob = new SicoobAPI(cfg);
+await sicoob.SetupAsync();
+
+var pixPeriodo = await sicoob.ConsultarPIX(new PIX.Sicoob.Lib.Models.Pix.ConsultaRequest()
+{
+    inicio = new System.DateTime(2023, 01, 01),
+    fim = new System.DateTime(2023, 01, 15),
+});
+
+var pix1 = await sicoob.ConsultarPIX(pixPeriodo.pix[0].endToEndId);
+
