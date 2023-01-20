36418017f20784f6417104a91945dad5ccf5fa82
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-20 09:29:06 -0300

    Utilizar UTC

--- PIX.Sicoob.Lib/Extensions/ToRFC.cs
+++ PIX.Sicoob.Lib/Extensions/ToRFC.cs
@@ -6,6 +6,6 @@ namespace PIX.Sicoob.Lib
     public static class ToRFC
     {
         public static string ToRFC3339(this DateTime dateTime)
-            => dateTime.ToString("yyyy-MM-dd'T'HH:mm:ss.fffzzz", DateTimeFormatInfo.InvariantInfo);
+            => dateTime.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss.fffzzz", DateTimeFormatInfo.InvariantInfo);
     }
 }
