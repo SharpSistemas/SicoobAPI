e93248d292d0ec12494b2887531ca910d4af1bb4
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-23 13:16:43 -0300

    Update Program.cs

--- Sicoob.PIX.Testes/Program.cs
+++ Sicoob.PIX.Testes/Program.cs
@@ -11,7 +11,15 @@ var cfg = JsonConvert.DeserializeObject<ConfiguracaoAPI>(File.ReadAllText("confi
 //File.WriteAllText("config.json", JsonConvert.SerializeObject(cfg));
 
 var sicoob = new SicoobPIX(cfg);
+// Configura acesso das APIs
 await sicoob.SetupAsync();
+// A autenticação dura 300 segundos (5min) e é auto renovado pela biblioteca
+// Cada chamada verifica e, se necessário, atualiza o token
+// Chame AtualizarCredenciaisAsync() para renovar manualmente
+// É possível consultar dados da vigência do Token nas propriedades:
+//  * sicoob.ExpiresIn
+//  * sicoob.ExpiresAtUTC
+//  * sicoob.Expired
 
 /* COB */
 var cobs = await sicoob.ListarCobrancasAsync(new Sicoob.PIX.Models.Cobranca.ConsultaRequest
