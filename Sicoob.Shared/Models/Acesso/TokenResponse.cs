0bed1e97dc6a5d53af5e2d69b7657cd5fb992ca7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-24 17:00:23 -0300

    Update TokenResponse.cs

--- Sicoob.Shared/Models/Acesso/TokenResponse.cs
+++ Sicoob.Shared/Models/Acesso/TokenResponse.cs
@@ -3,6 +3,7 @@
     public class TokenResponse
     { 
         public string? access_token { get; set; }
+        public string? id_token { get; set; }
         public int expires_in { get; set; }
         public int refresh_expires_in { get; set; }
         public string? token_type { get; set; }
