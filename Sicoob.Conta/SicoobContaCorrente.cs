55d3bf19df05c6d0fa22c3742997ad89c11ac6ed
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-24 13:44:57 -0300

    Adiciona projetos Sicoob.Cobranca e Sicoob.Conta

---
+++ Sicoob.Conta/SicoobContaCorrente.cs
@@ -0,0 +1,36 @@
+﻿using Sicoob.Shared.Models.Acesso;
+using Simple.API;
+using System.Net.Http;
+
+namespace Sicoob.Conta
+{
+    public sealed class SicoobContaCorrente : Shared.Sicoob
+    {
+        // Documentações
+        // > APIs tipo "Swagger":
+        //   https://developers.sicoob.com.br/#!/apis
+        // > APIs no POSTMAN:
+        //   https://documenter.getpostman.com/view/20565799/UzBnrmod#239d9f68-d646-4209-994a-cd564b6d6d1a
+
+        private ClientInfo clientApi;
+        public Shared.Models.ConfiguracaoAPI ConfigApi { get; }
+
+        public SicoobContaCorrente(Shared.Models.ConfiguracaoAPI configApi)
+            : base(configApi)
+        {
+            ConfigApi = configApi;
+        }
+
+        protected override void setupClients(HttpClientHandler handler)
+        {
+            clientApi = new ClientInfo(ConfigApi.UrlApi, handler);
+            clientApi.SetHeader("x-sicoob-clientid", ConfigApi.ClientId);
+        }
+
+        protected override void atualizaClients(TokenResponse token)
+        {
+            clientApi.SetAuthorizationBearer(token.access_token);
+        }
+
+    }
+}
