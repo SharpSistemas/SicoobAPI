da0d5c5d6d43db2743fa7d1a9672a657dc671ab2
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-24 17:00:41 -0300

    Adiciona base da Poupança

--- Sicoob.Conta/SicoobContaPoupanca.cs
+++ Sicoob.Conta/SicoobContaPoupanca.cs
@@ -27,14 +27,24 @@ namespace Sicoob.Conta
             clientApi = new ClientInfo(ConfigApi.UrlApi, handler);
             clientApi.SetHeader("x-sicoob-clientid", ConfigApi.ClientId);
         }
-
         protected override void atualizaClients(TokenResponse token)
         {
             clientApi.SetAuthorizationBearer(token.access_token);
         }
 
+        /// <summary>
+        /// Pesquisa as contas de poupança que pertencem a um CPF ou CNPJ.
+        /// </summary>
+        public async Task<string> ListarContasAsync()
+            => await ExecutaChamadaAsync(() => clientApi.GetAsync<string>($"/poupanca/v1/contas"));
+        /// <summary>
+        /// Consulta o extrato de um mês e ano de uma conta poupança.
+        /// </summary>
         public async Task<string> ObterSaldoAsync(string contaPoupanca)
             => await ExecutaChamadaAsync(() => clientApi.GetAsync<string>($"/poupanca/v1/contas/{contaPoupanca}/saldo"));
+        /// <summary>
+        /// Consulta o saldo atual de uma conta poupança.
+        /// </summary>
         public async Task<string> ObterExtratoAsync(int mes, int ano, string contaPoupanca)
             => await ExecutaChamadaAsync(() => clientApi.GetAsync<string>($"/poupanca/v1/contas/{contaPoupanca}/extrato/{mes}/{ano}"));
 
