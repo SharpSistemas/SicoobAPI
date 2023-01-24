a786b074755e65fa29550e581c64505cffa58964
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-24 15:58:30 -0300

    Precisa ter alguma coisa na classe

--- Sicoob.Testes/Program.cs
+++ Sicoob.Testes/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Sicoob.Testes;
 
+System.Console.WriteLine("Escolha o Teste");
+
 //await TestesApiPIX.Run();
 //await TestesApiConta.Run_ContaCorrente();
 //await TestesApiConta.Run_ContaPoupanca();
