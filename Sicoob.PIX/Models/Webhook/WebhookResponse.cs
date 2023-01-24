5fb50ee8cb335eee49a010ab0ed72f5f1958e8da
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-24 09:07:56 -0300

    Create WebhookResponse.cs

---
+++ Sicoob.PIX/Models/Webhook/WebhookResponse.cs
@@ -0,0 +1,18 @@
+﻿using Sicoob.Shared.Models.Geral;
+using System;
+
+namespace Sicoob.PIX.Models.Webhook
+{
+    public class WebhookListResponse
+    {
+        public ResponseParametros parametros { get; set; }
+        public WebhookResponse[] webhooks { get; set; }
+    }
+
+    public class WebhookResponse
+    {
+        public string webhookUrl { get; set; }
+        public string chave { get; set; }
+        public DateTime criacao { get; set; }
+    }
+}
