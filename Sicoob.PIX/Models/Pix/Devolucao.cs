d8efbb7af557809c5bf913cb8d068727b973c68b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-25 10:03:22 -0300

    Create Devolucao.cs

---
+++ Sicoob.PIX/Models/Pix/Devolucao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sicoob.PIX.Models.Pix
+{
+    public class DevolucaoResponse
+    {
+        public enum ListaStatus
+        {
+            EM_PROCESSAMENTO, 
+            DEVOLVIDO, 
+            NAO_REALIZADO,
+
+            DESCONHECIDO,
+        }
+
+        public string id { get; set; }
+        public string rtrId { get; set; }
+        public string valor { get; set; }
+        public Horario horario { get; set; }
+        /// <summary>
+        /// EM_PROCESSAMENTO, DEVOLVIDO, NAO_REALIZADO
+        /// </summary>
+        public string status { get; set; } 
+        public string motivo { get; set; }
+
+        public ListaStatus ObterStatus()
+        {
+            if (!Enum.TryParse(status, out ListaStatus result))
+            {
+                result = ListaStatus.DESCONHECIDO;
+            }
+
+            return result;
+        }
+
+        public class Horario
+        {
+            public DateTime solicitacao { get; set; }
+            public DateTime liquidacao { get; set; }
+        }
+    }
+
+}
