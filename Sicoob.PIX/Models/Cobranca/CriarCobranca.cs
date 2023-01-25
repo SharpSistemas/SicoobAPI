be0e6b494523abd6d1a2d4dd6563669d21c7f88f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-25 10:03:49 -0300

    Move model de cobrança

--- Sicoob.PIX/Models/Cobranca/CriarCobranca.cs
+++ Sicoob.PIX/Models/Cobranca/CriarCobranca.cs
@@ -35,34 +35,4 @@ namespace Sicoob.PIX.Models.Cobranca
             };
         }
     }
-
-    public class CriarCobrancaResponse
-    {
-        public CalendarioResponse calendario { get; set; }
-        public string txid { get; set; }
-        public int revisao { get; set; }
-        public NomeCpfCnpj devedor { get; set; }
-        public LocResponse loc { get; set; }
-        public string location { get; set; }
-        /// <summary>
-        /// ATIVA, CONCLUIDA, REMOVIDA_PELO_USUARIO_RECEBEDOR, REMOVIDA_PELO_PSP
-        /// </summary>
-        public string status { get; set; }
-        public Valor valor { get; set; }
-        public string brcode { get; set; }
-        public string chave { get; set; }
-        public string solicitacaoPagador { get; set; }
-        public NomeValor[] infoAdicionais { get; set; }
-
-
-        public CobrancaCompleta.ListaStatus ObterStatus()
-        {
-            if (!Enum.TryParse(status, out CobrancaCompleta.ListaStatus result))
-            {
-                result = CobrancaCompleta.ListaStatus.DESCONHECIDO;
-            }
-
-            return result;
-        }
-    }
 }
--- Sicoob.PIX/Models/Pix/ConsultaResponse.cs
+++ Sicoob.PIX/Models/Pix/ConsultaResponse.cs
@@ -18,7 +18,7 @@ namespace Sicoob.PIX.Models.Pix
         public DateTime horario { get; set; }
         public string nomePagador { get; set; }
         public NomeCpfCnpj pagador { get; set; }
-        public Devolucao[] devolucoes { get; set; }
+        public DevolucaoResponse[] devolucoes { get; set; }
 
         public override string ToString()
         {
@@ -31,19 +31,5 @@ namespace Sicoob.PIX.Models.Pix
             return $"{horario:g} {valor:C2} {nomePagador}{dev}";
         }
 
-        public class Devolucao
-        {
-            public string id { get; set; }
-            public string rtrId { get; set; }
-            public string valor { get; set; }
-            public DevolucaoHorario horario { get; set; }
-            public string status { get; set; } // EM_PROCESSAMENTO, DEVOLVIDO, NAO_REALIZADO
-            public string motivo { get; set; }
-        }
-        public class DevolucaoHorario
-        {
-            public DateTime solicitacao { get; set; }
-            public DateTime liquidacao { get; set; }
-        }
     }
 }
