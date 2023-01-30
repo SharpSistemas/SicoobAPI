873f5ad6c330a200dbea4f44a017478e8f08a55c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-30 15:06:23 -0300

    Exceção

---
+++ CS.BCB.PIX/Excecoes/ErroRequisicaoException.cs
@@ -0,0 +1,26 @@
+﻿/**************************************\
+ * Biblioteca C# para APIs do PIX     *
+ * Autor: Rafael Estevam              *
+ *        gh/SharpSistemas/SicoobAPI  *
+\**************************************/
+using CS.BCB.PIX.Models;
+using System;
+
+namespace CS.BCB.PIX.Excecoes
+{
+    public class ErroRequisicaoException : Exception
+    {
+        public ErroRequisicao DadosErro { get; }
+
+        public ErroRequisicaoException(ErroRequisicao erro)
+            : base(erro.Title)
+        {
+            DadosErro = erro;
+        }
+        public ErroRequisicaoException(ErroRequisicao erro, Exception innerException)
+            : base(erro.Title, innerException)
+        {
+            DadosErro = erro;
+        }
+    }
+}
