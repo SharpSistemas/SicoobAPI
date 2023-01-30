baa96d8732104e1893d82f5060b0db2a4a426c28
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-30 11:32:54 -0300

    Atualiza classe com interface

--- Sicoob.PIX/SicoobPIX.cs
+++ Sicoob.PIX/SicoobPIX.cs
@@ -3,6 +3,8 @@
  * Autor: Rafael Estevam              *
  *        gh/SharpSistemas/SicoobAPI  *
 \**************************************/
+using CS.BCB.PIX.Contratos;
+using CS.BCB.PIX.Models;
 using Sicoob.Shared;
 using Sicoob.Shared.Models.Acesso;
 using Simple.API;
@@ -16,7 +18,7 @@ namespace Sicoob.PIX
     /// <summary>
     /// Classe para comunicação com as APIs de PIX do Sicoob
     /// </summary>
-    public sealed class SicoobPIX : Shared.Sicoob
+    public sealed class SicoobPIX : Shared.Sicoob, IApiPix
     {
         // Documentações
         // > APIs tipo "Swagger":
@@ -59,36 +61,34 @@ namespace Sicoob.PIX
         }
 
         /* COB */
-
         /// <summary>
         /// Endpoint que cria uma cobrança imediata (QRCode Pix).
         /// </summary>
         /// <param name="transactionId">String, deve ter de 27 a 36 caracteres. Identificador único da cobrança Pix</param>
         /// <param name="cobranca">Dados para geração da cobrança imediata.</param>
         /// <returns>Cobrança imediata criada</returns>
-        public async Task<CS.BCB.PIX.Models.Cobranca> CriarCobrancaAsync(string transactionId, CS.BCB.PIX.Models.NovaCobranca cobranca)
+        public async Task<Cobranca> CriarCobrancaAsync(string transactionId, NovaCobranca cobranca)
         {
             validaTxID(transactionId);
-            return await ExecutaChamadaAsync(() => clientApi.PutAsync<CS.BCB.PIX.Models.Cobranca>($"/pix/api/v2/cob/{transactionId}", cobranca));
+            return await ExecutaChamadaAsync(() => clientApi.PutAsync<Cobranca>($"/pix/api/v2/cob/{transactionId}", cobranca));
         }
-
         /// <summary>
         /// Endpoint para criar uma cobrança imediata, neste caso, o txid deve ser definido pelo PSP.
         /// </summary>
         /// <param name="cobranca">Dados para geração da cobrança imediata.</param>
         /// <returns>Cobrança imediata criada</returns>
-        public async Task<CS.BCB.PIX.Models.Cobranca> CriarCobrancaAsync(CS.BCB.PIX.Models.NovaCobranca cobranca)
-            => await ExecutaChamadaAsync(() => clientApi.PostAsync<CS.BCB.PIX.Models.Cobranca>($"/pix/api/v2/cob", cobranca));
+        public async Task<Cobranca> CriarCobrancaAsync(NovaCobranca cobranca)
+            => await ExecutaChamadaAsync(() => clientApi.PostAsync<Cobranca>($"/pix/api/v2/cob", cobranca));
         /// <summary>
         /// Endpoint para revisar uma cobrança através de um determinado txid.
         /// </summary>
         /// <param name="transactionId">String, deve ter de 27 a 36 caracteres. Identificador único da cobrança Pix.</param>
         /// <param name="cobranca">Dados para geração da cobrança</param>
         /// <returns>Cobrança imediata revisada. A revisão deve ser incrementada em 1.</returns>
-        public async Task<CS.BCB.PIX.Models.Cobranca> RevisarCobrancaAsync(string transactionId, CS.BCB.PIX.Models.RevisarCobranca cobranca)
+        public async Task<Cobranca> RevisarCobrancaAsync(string transactionId, RevisarCobranca cobranca)
         {
             validaTxID(transactionId);
-            return await ExecutaChamadaAsync(() => clientApi.PatchAsync<CS.BCB.PIX.Models.Cobranca>($"/pix/api/v2/cob/{transactionId}", cobranca));
+            return await ExecutaChamadaAsync(() => clientApi.PatchAsync<Cobranca>($"/pix/api/v2/cob/{transactionId}", cobranca));
         }
 
         /// <summary>
@@ -97,22 +97,22 @@ namespace Sicoob.PIX
         /// <param name="transactionId">String, deve ter de 27 a 36 caracteres. Identificador único da cobrança Pix.</param>
         /// <param name="revisao">Revisao a ser consultada</param>
         /// <returns>Dados da cobrança imediata</returns>
-        public async Task<CS.BCB.PIX.Models.Cobranca> ConsultarCobrancaAsync(string transactionId, int? revisao = null)
+        public async Task<Cobranca> ConsultarCobrancaAsync(string transactionId, int? revisao = null)
         {
             validaTxID(transactionId);
 
             string url = $"/pix/api/v2/cob/{transactionId}";
             if (revisao.HasValue) url += $"?revisao={revisao.Value}";
 
-            return await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.Cobranca>(url));
+            return await ExecutaChamadaAsync(() => clientApi.GetAsync<Cobranca>(url));
         }
         /// <summary>
         /// Endpoint para consultar cobranças imediatas através de parâmetros como início, fim, cpf, cnpj e status.
         /// </summary>
         /// <param name="consulta">Dados da consulta</param>
         /// <returns>Lista de cobranças imediatas.</returns>
-        public async Task<CS.BCB.PIX.Models.ListagemCobrancaRecebida> ListarCobrancasAsync(CS.BCB.PIX.Models.ConsultarCobranca consulta)
-            => await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.ListagemCobrancaRecebida>("/pix/api/v2/cob", consulta.ToKVP()));
+        public async Task<ListagemCobrancaRecebida> ListarCobrancasAsync(ConsultarCobranca consulta)
+            => await ExecutaChamadaAsync(() => clientApi.GetAsync<ListagemCobrancaRecebida>("/pix/api/v2/cob", consulta.ToKVP()));
 
         /// <summary>
         /// Endpoint para gerar a imagem qrcode de uma cobrança através de um determinado txid.
@@ -137,21 +137,20 @@ namespace Sicoob.PIX
         /* PayloadLocation */
 
         /* PIX */
-
         /// <summary>
         /// Endpoint para consultar Pix recebidos
         /// </summary>
         /// <param name="consulta">Dados da consulta</param>
         /// <returns>Lista dos Pix recebidos de acordo com o critério de busca.</returns>
-        public async Task<CS.BCB.PIX.Models.ListagemPixRecebido> ListarPIXAsync(CS.BCB.PIX.Models.ConsultarPix consulta)
-            => await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.ListagemPixRecebido>("/pix/api/v2/pix", consulta.ToKVP()));
+        public async Task<ListagemPixRecebido> ListarPIXAsync(ConsultarPix consulta)
+            => await ExecutaChamadaAsync(() => clientApi.GetAsync<ListagemPixRecebido>("/pix/api/v2/pix", consulta.ToKVP()));
         /// <summary>
         /// Endpoint para consultar um Pix através de um e2eid.
         /// </summary>
         /// <param name="endToEndId">Id fim a fim da transação. Deve ter 32 caracteres.</param>
         /// <returns>Dados do Pix efetuado.</returns>
-        public async Task<CS.BCB.PIX.Models.PixRecebido> ConsultarPIXAsync(string endToEndId)
-             => await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.PixRecebido>($"/pix/api/v2/pix/{endToEndId}"));
+        public async Task<PixRecebido> ConsultarPIXAsync(string endToEndId)
+             => await ExecutaChamadaAsync(() => clientApi.GetAsync<PixRecebido>($"/pix/api/v2/pix/{endToEndId}"));
 
         /// <summary>
         /// Endpoint para solicitar uma devolução através de um e2eid do Pix e do ID da devolução.
@@ -162,10 +161,10 @@ namespace Sicoob.PIX
         /// <param name="idDevolucao">Id gerado pelo cliente para representar unicamente uma devolução.</param>
         /// <param name="valor">Valor a ser devolvido</param>
         /// <returns>Dados da devolução</returns>
-        public async Task<CS.BCB.PIX.Models.PixDevolucao> SolicitarPixDevlucaoAsync(string endToEndId, string idDevolucao, decimal valor)
+        public async Task<PixDevolucao> SolicitarDevlucaoPixAsync(string endToEndId, string idDevolucao, decimal valor)
         {
             string url = $"/pix/api/v2/pix/{endToEndId}/devolucao/{idDevolucao}";
-            return await ExecutaChamadaAsync(() => clientApi.PutAsync<CS.BCB.PIX.Models.PixDevolucao>(url, new { valor = valor.ToString("N2", CultureInfo.InvariantCulture) }));
+            return await ExecutaChamadaAsync(() => clientApi.PutAsync<PixDevolucao>(url, new { valor = valor.ToString("N2", CultureInfo.InvariantCulture) }));
         }
         /// <summary>
         /// Endpoint para consultar uma devolução através de um EndToEndID do Pix e do ID da devolução
@@ -173,10 +172,10 @@ namespace Sicoob.PIX
         /// <param name="endToEndId">Id fim a fim da transação.</param>
         /// <param name="idDevolucao">Id gerado pelo cliente para representar unicamente uma devolução.</param>
         /// <returns>Dados da devolução</returns>
-        public async Task<CS.BCB.PIX.Models.PixDevolucao> ConsultarPixDevlucaoAsync(string endToEndId, string idDevolucao)
+        public async Task<PixDevolucao> ConsultarDevlucaoPixAsync(string endToEndId, string idDevolucao)
         {
             string url = $"/pix/api/v2/pix/{endToEndId}/devolucao/{idDevolucao}";
-            return await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.PixDevolucao>(url));
+            return await ExecutaChamadaAsync(() => clientApi.GetAsync<PixDevolucao>(url));
         }
 
         /* Webhook */
@@ -193,13 +192,13 @@ namespace Sicoob.PIX
         /// <summary>
         /// Endpoint para consultar Webhooks cadastrados
         /// </summary>
-        public async Task<CS.BCB.PIX.Models.ListagemWebhookAtivo> ConsultarWebHooksAsync()
-            => await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.ListagemWebhookAtivo>("/pix/api/v2/webhook"));
+        public async Task<ListagemWebhookAtivo> ConsultarWebHooksAsync()
+            => await ExecutaChamadaAsync(() => clientApi.GetAsync<ListagemWebhookAtivo>("/pix/api/v2/webhook"));
         /// <summary>
         /// Endpoint para recuperação de informações sobre o Webhook Pix.
         /// </summary>
-        public async Task<CS.BCB.PIX.Models.WebhookAtivo> ConsultarWebHookAsync(string chave)
-            => await ExecutaChamadaAsync(() => clientApi.GetAsync<CS.BCB.PIX.Models.WebhookAtivo>($"/pix/api/v2/webhook/{chave}"));
+        public async Task<WebhookAtivo> ConsultarWebHookAsync(string chave)
+            => await ExecutaChamadaAsync(() => clientApi.GetAsync<WebhookAtivo>($"/pix/api/v2/webhook/{chave}"));
         /// <summary>
         /// Endpoint para cancelamento do webhook. Não é a única forma pela qual um webhook pode ser removido.
         /// </summary>
