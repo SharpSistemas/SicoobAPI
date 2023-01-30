265e49d718d5b20a64d1c54db501c76dca1ff41a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-30 09:32:55 -0300

    Adiciona Models de WebHook

---
+++ CS.BCB.PIX/Models/WebhookAtivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CS.BCB.PIX.Models
+{
+    public class ListagemWebhookAtivo
+    {
+        public ResponseParametros parametros { get; set; }
+        public WebhookAtivo[] webhooks { get; set; }
+    }
+    public class WebhookAtivo
+    {
+        public string webhookUrl { get; set; }
+        public string chave { get; set; }
+        public DateTime criacao { get; set; }
+    }
+}
---
+++ CS.BCB.PIX/Models/WebhookCallback.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS.BCB.PIX.Models
+{
+    public class WebhookCallback
+    {
+        public WebhookPixRequest[] pix { get; set; }
+    }
+    public class WebhookPixRequest
+    {
+        public string endToEndId { get; set; }
+        public string txid { get; set; }
+        public string valor { get; set; }
+        public DateTime horario { get; set; }
+        public WebhookPixDevolucaoRequest[] devolucoes { get; set; }
+    }
+    public class WebhookPixDevolucaoRequest
+    {
+        public string id { get; set; }
+        public string rtrId { get; set; }
+        public string valor { get; set; }
+        public object horario { get; set; } // Não testado
+        public string status { get; set; }
+        public string motivo { get; set; }
+    }
+}
